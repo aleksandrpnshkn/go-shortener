565fb5d70913c5d644195f5eae6e169b446f4e13
aleksandrpnshkn <aleksandrpnshkn@gmail.com>
2025-07-02 20:34:07 +0400

    test load

---
+++ internal/store/file_test.go
@@ -0,0 +1,28 @@
+package store
+
+import (
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFileStorage(t *testing.T) {
+	_, testFileName, _, _ := runtime.Caller(0)
+	testFilesDirPath := filepath.Dir(testFileName) + "/test_files"
+
+	t.Run("entries loaded", func(t *testing.T) {
+		fileStorage, err := NewFileStorage(testFilesDirPath + "/test_load.txt")
+		require.NoError(t, err)
+		defer fileStorage.Close()
+
+		assert.Equal(t, 2, fileStorage.lastId, "last id loaded")
+
+		originalURL, isFound := fileStorage.Get("test2")
+
+		assert.True(t, isFound, "entry found")
+		assert.Equal(t, "http://example2.com", originalURL, "original url loaded")
+	})
+}

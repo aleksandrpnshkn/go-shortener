7fa3dede8479c3e7ba0fa1848d2a5244c5eb29ca
aleksandrpnshkn <aleksandrpnshkn@gmail.com>
2025-08-16 17:37:43 +0400

    use identity for key

--- cmd/shortener/migrations/000001_create_urls_table.up.sql
+++ cmd/shortener/migrations/000001_create_urls_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE urls (
-    id SERIAL PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
     code VARCHAR(8) NOT NULL UNIQUE,
     -- Примерный лимит по инфе из https://stackoverflow.com/a/417184
     original_url VARCHAR(2048) NOT NULL

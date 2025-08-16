614604c08b28415bae9fb214b91305e4512b27c7
aleksandrpnshkn <aleksandrpnshkn@gmail.com>
2025-08-16 20:40:20 +0400

    squash migrations

--- cmd/shortener/migrations/000001_create_urls_table.up.sql
+++ cmd/shortener/migrations/000001_create_urls_table.up.sql
@@ -2,5 +2,5 @@ CREATE TABLE urls (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     code VARCHAR(8) NOT NULL UNIQUE,
     -- Примерный лимит по инфе из https://stackoverflow.com/a/417184
-    original_url VARCHAR(2048) NOT NULL
+    original_url VARCHAR(2048) NOT NULL UNIQUE
 );
--- cmd/shortener/migrations/000002_make_original_url_unique.up.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE urls ADD CONSTRAINT urls_original_url_unique UNIQUE (original_url);

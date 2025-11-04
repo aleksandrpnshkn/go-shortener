ea5ae72af23c5132c7fa176f0ef7ae0c4cf99f85
aleksandrpnshkn <aleksandrpnshkn@gmail.com>
2025-11-04 19:12:03 +0400

    remove useless var

--- internal/store/users/sql.go
+++ internal/store/users/sql.go
@@ -33,17 +33,15 @@ func (s *SQLStorage) Get(ctx context.Context, userID int64) (*User, error) {
 }
 
 func (s *SQLStorage) Create(ctx context.Context) (*User, error) {
-	var userID int64
+	var user User
 
 	row := s.pgxpool.QueryRow(ctx, "INSERT INTO users (id) VALUES (DEFAULT) RETURNING id")
-	err := row.Scan(&userID)
+	err := row.Scan(&user.ID)
 	if err != nil {
 		return nil, err
 	}
 
-	return &User{
-		ID: userID,
-	}, nil
+	return &user, nil
 }
 
 func (s *SQLStorage) Close() error {
